f0d8a2143a3bccc097a7f52d77b63c3ef233d7ac
Lincoln Hoey Moore <johlinco@gmail.com>
2024-09-01 20:12:03 -0400

    Setting up 3.9

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
+
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
@@ -95,7 +100,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
