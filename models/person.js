7180c1219ac4fb7b732bd8c2b0d7c0290972b266
Lincoln Hoey Moore <johlinco@gmail.com>
2024-09-05 13:13:51 -0400

    Completing 3.19

--- models/person.js
+++ models/person.js
@@ -19,7 +19,17 @@ const personSchema = new mongoose.Schema({
         type: String,
         minLength: 3,
     },
-    number: String
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(input) {
+                return /\d{2}-\d/.test(input) || /\d{3}-\d/.test(input)
+            },
+            message: props => `${props.value} is not a valid phone number. Use format 00-00000 or 000-00000`
+        },
+        required: [true, 'Must provide phone number for phone book entry']
+    }
 })
 
 personSchema.set('toJSON', {
